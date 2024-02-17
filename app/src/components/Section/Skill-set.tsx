af53abfc812cfe4b3045c723bdd9201942a8f84a
Romiono <roma.serebrennikov.00@mail.ru>
2024-02-17 12:17:08 +0300

    skill-set skill-card

--- app/src/components/Section/Skill-set.tsx
+++ app/src/components/Section/Skill-set.tsx
@@ -1,6 +1,9 @@
 import styled from "styled-components";
-import SkillCard from "../UI/Skill-card/Skill-card.tsx";
+import SkillCard, {ISkillCard} from "../UI/Skill-card/Skill-card.tsx";
 import progress from "../../assets/UI/Group 1.svg";
+// import {FolderArrowDownIcon} from '@heroicons/react/24/outline'
+import git from '../../assets/skill-cards/git.svg'
+
 
 const SkillSet = () => {
 
@@ -17,21 +20,18 @@ const SkillSet = () => {
         border-radius: 3px;
     }
 `;
-
+    const cards: ISkillCard[] = [
+        {title: 'github', description: 'Контроль версий', icon: git}
+    ]
     return (
         <div className={'container mx-auto px-8'}>
             <h2 className={'text-5xl text-accent mb-10'}><Span>Skill</Span>-Set</h2>
             <div className={'columns-2 flex flex-row'}>
                 {/*<div className={'columns-3 basis-11/12'}>*/}
-                <div className={'basis-6/12 grid grid-cols-3'}>
-                    <SkillCard/>
-                    <SkillCard/>
-                    <SkillCard/>
-                    <SkillCard/>
-                    <SkillCard/>
-                    <SkillCard/>
+                <div className={'basis-7/12 grid grid-cols-3'}>
+                    {cards.map((card, index) => <SkillCard icon={card.icon} key={index} title={card.title} description={card.description}/>)}
                 </div>
-                <div className={'basis-6/12 flex justify-end items-center'}>
+                <div className={'basis-5/12 flex justify-end items-center'}>
                     <img className={'h-52'} src={progress} alt={'progress'}/>
                 </div>
             </div>
--- app/src/components/UI/Skill-card/Skill-card.tsx
+++ app/src/components/UI/Skill-card/Skill-card.tsx
@@ -1,6 +1,15 @@
-import {FolderArrowDownIcon} from '@heroicons/react/24/outline'
+// import {FolderArrowDownIcon} from '@heroicons/react/24/outline'
 import styled from "styled-components";
-const SkillCard = () => {
+import {useState} from "react";
+export interface ISkillCard {
+    icon: string
+    title: string
+    description: string
+}
+
+const SkillCard = ({title, icon, description}: ISkillCard) => {
+
+    const [flip, setFlip] = useState<boolean>(false)
 
     const Icon = styled.div`
         width: 90%;
@@ -8,13 +17,22 @@ const SkillCard = () => {
         margin: 5px;
     `;
     return (
-        <div>
-            <Icon className={'grid content-center bg-header'}>
-                <FolderArrowDownIcon/>
-            </Icon>
-            <div>
-
-            </div>
+        <div onClick={() => setFlip(!flip)}>
+            {flip ?
+                <div>
+                    <div>
+                        {/*<svg path={icon} className={'w-full h-full'}/>*/}
+                        <img src={icon} alt={'card-logo'} />
+                        <h6>{title}</h6>
+                        <p>{description}</p>
+                    </div>
+                </div> :
+                <Icon className={'grid justify-center items-center bg-header'}>
+                    {/*<svg path={icon} className={'w-full'}/>*/}
+                    {/*<FolderArrowDownIcon/>*/}
+                    <img src={icon} alt={'card-logo'}/>
+                </Icon>
+            }
         </div>
     );
 };
